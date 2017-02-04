d7e2a27666532d3010ae9ac20aa7a638ffddf433
maxim <maxim@webro.com.ua>
2017-02-04 13:51:55 +0200

    commands to worker

---
+++ commands/worker.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/**
+ * Created by maxim on 04.02.17.
+ */
+
+self.addEventListener('message', function(e) {
+	var data = e.data;
+	switch (data.cmd) {
+		case 'start':
+			self.postMessage('WORKER STARTED: ' + data.msg);
+			break;
+		case 'stop':
+			self.postMessage('WORKER STOPPED: ' + data.msg +
+				'. (buttons will no longer work)');
+			self.close(); // Terminates the worker.
+			break;
+		default:
+			self.postMessage('Unknown command: ' + data.msg);
+	};
+}, false);
